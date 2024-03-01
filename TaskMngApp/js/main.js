800f6c87826febcf6d0b92ba8ea5928f9e971dd9
AnhSon-DHSPDHDN <anhson2121999@gmail.com>
2024-03-01 22:24:22 +0700

    Update Task management app

--- TaskMngApp/js/main.js
+++ TaskMngApp/js/main.js
@@ -1,25 +1,9 @@
 // Task sẽ có các thuộc tính: taskName, createAt, createBy, id
 
-let allTask = [
-  {
-    taskName: "An com",
-    createAt: new Date(),
-    createBy: "Son",
-    id: 1,
-  },
-  {
-    taskName: "Di ngu",
-    createAt: new Date(),
-    createBy: "Son",
-    id: 2,
-  },
-  {
-    taskName: "Thuc day",
-    createAt: new Date(),
-    createBy: "Son",
-    id: 3,
-  },
-]; // Lưu danh sách các tasks
+const KEY_TASKS_LIST = "tasks";
+let allTask = JSON.parse(localStorage.getItem(KEY_TASKS_LIST)) || []; // Lưu danh sách các tasks
+let taskEdit = null;
+// Trong trường hợp localStorage.getItem = null thì gán mảng rỗng cho allTask
 
 function addTask(taskName, createBy) {
   const task = {
@@ -30,6 +14,8 @@ function addTask(taskName, createBy) {
   };
 
   allTask.push(task);
+  // Lưu all task vào localStorage
+  localStorage.setItem(KEY_TASKS_LIST, JSON.stringify(allTask));
 }
 
 function renderTasks() {
@@ -44,6 +30,7 @@ function renderTasks() {
         <td>${task.createAt}</td>
         <td>
           <button onclick="deleteTaskById(${task.id})">Del</button>
+          <button onclick="showTaskEdit(${task.id})">Edit</button>
         </td>
       </tr>`;
   });
@@ -51,17 +38,69 @@ function renderTasks() {
   tbodyElement.innerHTML = bodyTableHtml;
 }
 
+function updateContentBtnAddTask() {
+  const btnAddTask = document.querySelector(".js-btn-add-task");
+
+  if (taskEdit) {
+    btnAddTask.textContent = "Edit Task";
+  } else {
+    btnAddTask.textContent = "Add Task";
+  }
+}
+
+function showTaskEdit(taskId) {
+  const existedTaskEdit = allTask.find((task) => task.id === taskId);
+
+  if (existedTaskEdit) {
+    console.log("existedTaskEdit", existedTaskEdit);
+    const inputTaskEle = document.querySelector(".js-input-task-name");
+    taskEdit = { ...existedTaskEdit };
+    inputTaskEle.value = taskEdit.taskName;
+    updateContentBtnAddTask();
+  }
+}
+
 function deleteTaskById(taskId) {
   console.log("delete task", taskId);
+  if (taskEdit?.id === taskId) {
+    // Nếu task đang được chỉnh sửa thì chặn không cho xoá
+    alert("Task đang được chỉnh sửa");
+    return;
+  }
+
   allTask = allTask.filter((task) => task.id !== taskId);
+
+  localStorage.setItem(KEY_TASKS_LIST, JSON.stringify(allTask));
   renderTasks();
+}
+
+function editTask(newValue) {
+  const indexTaskEdit = allTask.findIndex((task) => task.id === taskEdit.id);
+
+  if (indexTaskEdit !== -1) {
+    // Trong trường hợp indexTaskEdit khác -1 => tồn tại giá trị thoã mãn trong allTask
+    allTask[indexTaskEdit] = {
+      ...taskEdit,
+      taskName: newValue,
+    };
+    renderTasks();
+
+    // Đồng bộ allTask với localStorage
+    localStorage.setItem(KEY_TASKS_LIST, JSON.stringify(allTask));
+    taskEdit = null;
+    updateContentBtnAddTask();
+  }
 }
 
 function handleAddTask(event) {
   event.preventDefault();
   const inputTaskEle = document.querySelector(".js-input-task-name");
 
-  addTask(inputTaskEle.value, "Son");
+  if (taskEdit) {
+    editTask(inputTaskEle.value);
+  } else {
+    addTask(inputTaskEle.value, "Son");
+  }
   renderTasks();
   inputTaskEle.value = "";
 }
